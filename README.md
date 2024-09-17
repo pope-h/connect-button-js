5d3096bf545ace6025962f0ad872c1ac909e03bf
pope-h <hamatthan@gmail.com>
2024-09-16 18:56:28 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+# IMPORTANT INFO
+This project was done in order to remind on how to quickly set up the connect wallet using web3modal in forgotten situations.
+
+- First Sign in to https://cloud.walletconnect.com/app and get your project ID
+- Set up a React JS project with npm `create vite@latest` and follow the prompts and instructions after installation.
+- Finally follow the instructions at [ethers-installation](https://docs.walletconnect.com/appkit/react/core/installation?platform=ethers)
---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react";
+import ConnectButton from "./components/ConnectButton";
+import { useState } from "react";
+import styles from "./styles/Home.module.css";
+
+// 1. Get projectId
+const projectId = "57c3ed3f7633af987eda789d503edfee"; // project ID gotten from a sample project
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: "Ethereum",
+  currency: "ETH",
+  explorerUrl: "https://etherscan.io",
+  rpcUrl: "https://cloudflare-eth.com",
+};
+
+// 3. Create a metadata object
+const metadata = {
+  name: "My Website",
+  description: "My Website description",
+  url: "https://mywebsite.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.mywebsite.com/"],
+};
+
+// 4. Create Ethers config
+const ethersConfig = defaultConfig({
+  /*Required*/
+  metadata,
+
+  /*Optional*/
+  enableEIP6963: true, // true by default
+  enableInjected: true, // true by default
+  enableCoinbase: true, // true by default
+  rpcUrl: "...", // used for the Coinbase SDK
+  defaultChainId: 1, // used for the Coinbase SDK
+});
+
+// 5. Create a AppKit instance
+createWeb3Modal({
+  ethersConfig,
+  chains: [mainnet],
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
+
+export default function App() {
+  const [isNetworkSwitchHighlighted, setIsNetworkSwitchHighlighted] =
+    useState(false);
+  const [isConnectHighlighted, setIsConnectHighlighted] = useState(false);
+
+  const closeAll = () => {
+    setIsNetworkSwitchHighlighted(false);
+    setIsConnectHighlighted(false);
+  };
+  return (
+    <>
+      <header>
+        <div
+          className={styles.backdrop}
+          style={{
+            opacity: isConnectHighlighted || isNetworkSwitchHighlighted ? 1 : 0,
+          }}
+        />
+        <div className={styles.header}>
+          <div className={styles.buttons}>
+            <div
+              onClick={closeAll}
+              className={`${styles.highlight} ${
+                isNetworkSwitchHighlighted ? styles.highlightSelected : ``
+              }`}
+            >
+              <w3m-network-button />
+            </div>
+            <div
+              onClick={closeAll}
+              className={`${styles.highlight} ${
+                isConnectHighlighted ? styles.highlightSelected : ``
+              }`}
+            >
+              <ConnectButton />
+              {/* Could also use <w3m-button /> */}
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
